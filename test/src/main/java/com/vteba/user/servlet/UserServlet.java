c2614f46b22736f4607c591be23cfbe00d257e47
skmbw <tongku2008@126.com>
2014-07-05 13:41:23 +0800

    打印测试时间，调整日志级别

--- test/src/main/java/com/vteba/user/servlet/UserServlet.java
+++ test/src/main/java/com/vteba/user/servlet/UserServlet.java
@@ -36,28 +36,35 @@ public class UserServlet extends AutowiredHttpServlet {
         user.setPassword("123456");
         user.setState(true);
         user.setUserName("yinlei");
-        
+        long dd = System.currentTimeMillis();
         Long id = userDaoImpl.save(user);
-        System.out.println(id);
+        System.out.println("save时间：" + (System.currentTimeMillis() - dd));
+        //System.out.println(id);
         
+        long dd2 = System.currentTimeMillis();
         user = userDaoImpl.get(id);
-        System.out.println(user);
+        System.out.println("get时间：" + (System.currentTimeMillis() - dd2));
+        //System.out.println(user);
         
         user = userDaoImpl.unique(user);
-        System.out.println(user);
+        //System.out.println(user);
         
         User entity = new User();
         entity.setId(id);
         entity.setAge(19);
         entity.setEmail("yinlei@126.com");
+        long d = System.currentTimeMillis();
         int i = userDaoImpl.update(entity);
+        System.out.println("update时间：" + (System.currentTimeMillis() - d));
         System.out.println("更新条数：" + i);
         
         User criteria = new User();
         criteria.setId(5L);
         entity.setId(null);
+        long d1 = System.currentTimeMillis();
         i = userDaoImpl.updateBatch(entity, criteria);
-        System.out.println("updateBatch：" + i);
+        System.out.println("updateBatch时间：" + (System.currentTimeMillis() - d1));
+        //System.out.println("updateBatch：" + i);
         
         Page<User> page = new Page<User>();
         Map<String, String> orders = Maps.newHashMap();
@@ -68,21 +75,24 @@ public class UserServlet extends AutowiredHttpServlet {
         User params = new User();
         params.setAge(14);
         params.setState(true);
+        long d2 = System.currentTimeMillis();
         page = userDaoImpl.queryForPage(page, params);
-        System.out.println(page);
+        System.out.println("queryForPage时间：" + (System.currentTimeMillis() - d2));
+        //System.out.println(page);
         
         String sql = "select * from user where age = ? and state = 1";
         userDaoImpl.queryForPage(page, sql, 14);
         
         userDaoImpl.query(sql, 14);
-        
+        long d3 = System.currentTimeMillis();
         userDaoImpl.query(params);
-        
+        System.out.println("query时间：" + (System.currentTimeMillis() - d3));
         sql = "select * from user where age = :age and state = 1";;
         userDaoImpl.query(sql, params);
         
+        long d4 = System.currentTimeMillis();
         userDaoImpl.queryList(sql, params);
-        
+        System.out.println("queryList时间：" + (System.currentTimeMillis() - d4));
         userDaoImpl.queryPageList(page, sql, params);
         
         sql = "select * from user where age = ? and state = 1";
@@ -94,8 +104,9 @@ public class UserServlet extends AutowiredHttpServlet {
         sql = "select age from user where id = :id";
         Map<String, Object> paramMap = Maps.newHashMap();
         paramMap.put("id", 14);
+        long d5 = System.currentTimeMillis();
         userDaoImpl.queryForObject(sql, paramMap, Integer.class);
-        
+        System.out.println("queryForObject时间：" + (System.currentTimeMillis() - d5));
 //        int ii = userDaoImpl.delete(id);
 //        System.out.println("删除条数：" + ii);
         

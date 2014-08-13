e3fd83917838f2c7e552962267c2bfcadd644b2f
skmbw <tongku2008@126.com>
2014-08-13 18:29:01 +0800

    代码生成工具测试

--- test/src/test/java/com/vteba/test/TestCoder.java
+++ test/src/test/java/com/vteba/test/TestCoder.java
@@ -1,20 +1,26 @@
 package com.vteba.test;
 
 import com.vteba.ext.codegen.CodeBuilder;
+import com.vteba.ext.codegen.DB;
 import com.vteba.ext.codegen.KeyType;
+import com.vteba.ext.codegen.TempType;
 
 public class TestCoder {
 
 	public static void main(String[] args) {
 	    //项目绝对路径
 		String rootPath = "C:\\Users\\Administrator\\git\\test\\test\\";
-		CodeBuilder builder = new CodeBuilder(rootPath);
-		builder.schema("skmbw")
+		CodeBuilder builder = new CodeBuilder(rootPath, TempType.Generic);
+		builder.setConfigFilePath("src/main/resources/config.properties")
+		.schema("skmbw")
 		.className("EmpUser")
+		.setDb(DB.MySQL)
 		.keyType(KeyType.Integer)
 		.tableDesc("系统用户")
 		.tableName("emp_user")
-		.module("user2")
+		.module("com.vteba.user2")
+		.setPojo(false)
+		.setMongo(false)
 		.build();
 
 	}

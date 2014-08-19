f31d57c4a5d133550e5bc0dfbdbaadb875897aff
skmbw <tongku2008@126.com>
2014-08-19 18:02:16 +0800

    产生pdf

---
+++ test/src/test/java/com/vteba/test/Demo4HTMLFile2PDF.java
@@ -0,0 +1,185 @@
+package com.vteba.test;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.itextpdf.text.BaseColor;
+import com.itextpdf.text.Chapter;
+import com.itextpdf.text.Chunk;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.Element;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.PageSize;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.Section;
+import com.itextpdf.text.WritableDirectElement;
+import com.itextpdf.text.pdf.BaseFont;
+import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.text.pdf.draw.LineSeparator;
+import com.itextpdf.tool.xml.ElementHandler;
+import com.itextpdf.tool.xml.Writable;
+import com.itextpdf.tool.xml.XMLWorkerHelper;
+import com.itextpdf.tool.xml.pipeline.WritableElement;
+
+/**
+ * HTML文件转换为PDF
+ *
+ * @author <a href="http://www.micmiu.com">Michael Sun</a>
+ */
+public class Demo4HTMLFile2PDF {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) throws Exception {
+        String htmlFile = "d:/dd.html";
+
+        // 直接把HTML文件转为PDF文件
+        String pdfFile = "d:/demo-htmlfile.pdf";
+        Demo4HTMLFile2PDF.parseHTML2PDFFile(pdfFile, new FileInputStream(
+                htmlFile));
+
+        // HTML文件转为PDF中的Elements
+        String pdfFile2 = "d:/demo-htmlfile2.pdf";
+        Demo4HTMLFile2PDF.parseHTML2PDFElement(pdfFile2, new FileInputStream(
+                htmlFile));
+    }
+
+    /**
+     * 用于HTML直接转换为PDF文件
+     *
+     * @param fileName
+     * @throws Exception
+     */
+    public static void parseHTML2PDFFile(String pdfFile,
+            InputStream htmlFileStream) throws Exception {
+
+        BaseFont bfCN = BaseFont.createFont("STSongStd-Light", "UniGB-UCS2-H",
+                false);
+        // 中文字体定义
+        Font chFont = new Font(bfCN, 12, Font.NORMAL, BaseColor.BLUE);
+        Font secFont = new Font(bfCN, 12, Font.NORMAL, new BaseColor(0, 204,
+                255));
+
+        Document document = new Document();
+        PdfWriter pdfwriter = PdfWriter.getInstance(document,
+                new FileOutputStream(pdfFile));
+        pdfwriter.setViewerPreferences(PdfWriter.HideToolbar);
+        document.open();
+
+        int chNum = 1;
+        Chapter chapter = new Chapter(new Paragraph("HTML文件转PDF测试", chFont),
+                chNum++);
+
+        Section section = chapter.addSection(new Paragraph("/dev/null 2>&1 详解",
+                secFont));
+        // section.setNumberDepth(2);
+        // section.setBookmarkTitle("基本信息");
+        section.setIndentation(10);
+        section.setIndentationLeft(10);
+        section.setBookmarkOpen(false);
+        section.setNumberStyle(Section.NUMBERSTYLE_DOTTED_WITHOUT_FINAL_DOT);
+        section.add(Chunk.NEWLINE);
+        document.add(chapter);
+
+        // html文件
+        InputStreamReader isr = new InputStreamReader(htmlFileStream, "UTF-8");
+
+        // 方法一：默认参数转换
+        XMLWorkerHelper.getInstance().parseXHtml(pdfwriter, document, isr);
+
+        // 方法二：可以自定义参数
+        // HtmlPipelineContext htmlContext = new HtmlPipelineContext(null);
+        // htmlContext.charSet(Charset.forName("UTF-8"));
+        // htmlContext.setTagFactory(Tags.getHtmlTagProcessorFactory());
+        // CSSResolver cssResolver = XMLWorkerHelper.getInstance()
+        // .getDefaultCssResolver(true);
+        // Pipeline<?> pipeline = new CssResolverPipeline(cssResolver,
+        // new HtmlPipeline(htmlContext, new PdfWriterPipeline(document,
+        // pdfwriter)));
+        // XMLWorker worker = new XMLWorker(pipeline, true);
+        // XMLParser p = new XMLParser();
+        // p.addListener(worker);
+        //
+        // p.parse(isr);
+        // p.flush();
+
+        document.close();
+    }
+
+    /**
+     * HTML文件转为PDF中的Elements,便于把HTML内容追加到已有的PDF中
+     *
+     * @param pdfFile
+     * @param htmlFileStream
+     */
+    public static void parseHTML2PDFElement(String pdfFile,
+            InputStream htmlFileStream) {
+        try {
+            Document document = new Document(PageSize.A4);
+
+            FileOutputStream outputStream = new FileOutputStream(pdfFile);
+            PdfWriter pdfwriter = PdfWriter.getInstance(document, outputStream);
+            // pdfwriter.setViewerPreferences(PdfWriter.HideToolbar);
+            document.open();
+
+            BaseFont bfCN = BaseFont.createFont("STSongStd-Light", "UniGB-UCS2-H", false);
+            // 中文字体定义
+            Font chFont = new Font(bfCN, 12, Font.NORMAL, BaseColor.BLUE);
+            Font secFont = new Font(bfCN, 12, Font.NORMAL, new BaseColor(0, 204, 255));
+            Font textFont = new Font(bfCN, 12, Font.NORMAL, BaseColor.BLACK);
+
+            int chNum = 1;
+            Chapter chapter = new Chapter(new Paragraph("HTML文件转PDF元素，便于追加其他内容", chFont), chNum++);
+
+            Section section = chapter.addSection(new Paragraph("/dev/null 2>&1 详解", secFont));
+
+            section.setIndentation(10);
+            section.setIndentationLeft(10);
+            section.setBookmarkOpen(false);
+            section.setNumberStyle(Section.NUMBERSTYLE_DOTTED_WITHOUT_FINAL_DOT);
+            section.add(Chunk.NEWLINE);
+
+            final List<Element> pdfeleList = new ArrayList<Element>();
+            ElementHandler elemH = new ElementHandler() {
+
+                public void add(final Writable w) {
+                    if (w instanceof WritableElement) {
+                        pdfeleList.addAll(((WritableElement) w).elements());
+                    }
+
+                }
+            };
+            InputStreamReader isr = new InputStreamReader(htmlFileStream, "UTF-8");
+            XMLWorkerHelper.getInstance().parseXHtml(elemH, isr);
+
+            List<Element> list = new ArrayList<Element>();
+            for (Element ele : pdfeleList) {
+                if (ele instanceof LineSeparator
+                        || ele instanceof WritableDirectElement) {
+                    continue;
+                }
+                list.add(ele);
+            }
+            section.addAll(list);
+
+            section = chapter.addSection(new Paragraph("继续添加章节", secFont));
+
+            section.setIndentation(10);
+            section.setIndentationLeft(10);
+            section.setBookmarkOpen(false);
+            section.setNumberStyle(Section.NUMBERSTYLE_DOTTED_WITHOUT_FINAL_DOT);
+            section.add(new Chunk("测试HTML转为PDF元素，方便追加其他内容", textFont));
+
+            document.add(chapter);
+            document.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
---
+++ test/src/test/java/com/vteba/test/PdfGenerator.java
@@ -0,0 +1,210 @@
+package com.vteba.test;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.StringReader;
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.itextpdf.text.Chunk;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Element;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.html.simpleparser.HTMLWorker;
+import com.itextpdf.text.html.simpleparser.StyleSheet;
+import com.itextpdf.text.pdf.AcroFields;
+import com.itextpdf.text.pdf.BaseFont;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfReader;
+import com.itextpdf.text.pdf.PdfStamper;
+import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.tool.xml.Pipeline;
+import com.itextpdf.tool.xml.XMLWorker;
+import com.itextpdf.tool.xml.XMLWorkerHelper;
+
+public class PdfGenerator {
+    /**
+     * 通过PDF模板生成PDF
+     * @param tempFilePath
+     * @param targetFilePath
+     * @param fieldValueMap
+     * @return
+     */
+    public boolean generatePdfByTemplate(String tempFilePath,String targetFilePath, Map<String, String> fieldValueMap) {
+        boolean isNewTargetFile = true;
+        try {
+            File tempFile = new File(tempFilePath);
+            if (!tempFile.exists()) {
+                throw new FileNotFoundException(tempFilePath + " 不存在...");
+            }
+            // 删除已有文件
+            File targetFile = new File(targetFilePath);
+            if (targetFile.exists()) {
+                targetFile.delete();
+            }
+            PdfReader reader = new PdfReader(tempFilePath);// 模版文件目录
+            PdfStamper ps = new PdfStamper(reader, new FileOutputStream(targetFilePath)); // 生成的输出流
+            AcroFields s = ps.getAcroFields();// 获取pdf模板中所有的文本域
+
+            BaseFont bfChinese = BaseFont.createFont("STSong-Light",
+                    "UniGB-UCS2-H", BaseFont.NOT_EMBEDDED);
+            for (Object key : fieldValueMap.keySet()) {
+                s.setFieldProperty(key.toString(), "textfont", bfChinese, null);// 设置中文字符集
+                s.setField(key.toString(), fieldValueMap.get(key));
+            }
+            ps.setFormFlattening(true);// 这句不能少
+            ps.close();
+            reader.close();
+        } catch (FileNotFoundException e) {
+            isNewTargetFile = false;
+            e.printStackTrace();
+        } catch (IOException e) {
+            isNewTargetFile = false;
+            e.printStackTrace();
+        } catch (DocumentException e) {
+            isNewTargetFile = false;
+            e.printStackTrace();
+        }finally{
+        }
+        return isNewTargetFile;
+    }
+    /**
+     * 通过File对象生成PDF
+     * @param targetFilePath
+     * @param htmlFile
+     * @return
+     */
+    public boolean generatePdfByHtml(String targetFilePath, File htmlFile) {
+        boolean isSuccsee = false;
+        BufferedReader br;
+        try {
+            br = new BufferedReader(new InputStreamReader(new FileInputStream(htmlFile), "utf-8"));
+            String data = null;
+            StringBuffer b = new StringBuffer();
+            while ((data = br.readLine()) != null) {
+                b.append(data);
+            }
+            br.close();
+            isSuccsee = generatePdfByHtml(targetFilePath, b.toString());
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }  
+        return isSuccsee;
+    }
+    /**
+     * 通过HTML字符串生成PDF
+     * @param targetFilePath
+     * @param htmlStr
+     * @return
+     */
+    public boolean generatePdfByHtml(String targetFilePath, String htmlStr) {
+        boolean isSuccsee = false;
+        isSuccsee = generatePdfByHtml(null,null,targetFilePath, htmlStr, false);
+        return isSuccsee;
+    }
+
+    public boolean generatePdfByHtml(Document doc,Font fontChinese,String targetFilePath, String htmlStr,boolean isSetVersion) {
+        boolean isSuccsee = false;
+        OutputStream os;
+        try {
+            os = new FileOutputStream(targetFilePath);
+            if(null==doc){
+                doc = new Document();
+            }
+            if(null==fontChinese){
+                //中文字符设置
+                BaseFont baseFontChinese = BaseFont.createFont("STSong-Light","UniGB-UCS2-H", BaseFont.NOT_EMBEDDED);
+                fontChinese = new Font(baseFontChinese, 12, Font.NORMAL);
+            }
+            //创建监听器
+            PdfWriter writer = PdfWriter.getInstance(doc, os);
+            if (isSetVersion) {
+                //加权限 
+                writer.setPdfVersion(writer.PDF_VERSION_1_7);
+                int perssion = writer.ALLOW_PRINTING | writer.HideMenubar
+                        | writer.HideToolbar | writer.HideWindowUI | writer.ALLOW_FILL_IN;
+                writer.setEncryption(null, null, perssion,writer.STANDARD_ENCRYPTION_40);
+            }
+            Paragraph context = new Paragraph();
+            //context.add(htmlStr);
+            
+            FileInputStream fis = new FileInputStream("d:/bb.xml");
+            
+            doc.setPageCount(1);
+            
+            XMLWorkerHelper.getInstance().parseXHtml(writer, doc, fis);
+            
+            
+            
+            
+//            StyleSheet ss = new StyleSheet();
+//            List htmlList = HTMLWorker.parseToList(new StringReader(htmlStr),ss);
+//            for (int i = 0; i < htmlList.size(); i++) {
+//                Element e = (Element) htmlList.get(i);
+//                // 为表格里面的内容设字体
+//                if (htmlList.get(i) instanceof PdfPTable) {
+//                    PdfPTable pt = (PdfPTable) htmlList.get(i);
+//                    for (int j = 0; j < pt.getRows().size(); j++) {
+//                        for (PdfPCell item : pt.getRow(j).getCells()) {
+//                            if (item != null) {
+//                                item.getCompositeElements();
+//                                for (Element el : item.getCompositeElements()) {
+//                                    for (Chunk ck : el.getChunks()) {
+//                                        ck.setFont(fontChinese);
+//                                    }
+//                                }
+//                            }
+//                        }
+//                    }
+//                } else {
+//                    context.setFont(fontChinese);
+//                }
+//                for (Chunk ck : e.getChunks()) {
+//                    ck.setFont(fontChinese);
+//                }
+//                context.add(e);
+//            }
+            doc.open();
+            doc.add(context);
+            doc.close();
+            isSuccsee = true;
+        } catch (DocumentException e1) {
+            e1.printStackTrace();
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+        return isSuccsee;
+    }
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        String tempFilePath = "d:/FirstPdf0.pdf";
+        String targetFilePath = "d:/FirstPdf1.pdf";
+        Map<String, String> filedValueMap = new HashMap<String, String>();
+        filedValueMap.put("t0", "this is t0    好好");
+        filedValueMap.put("Text1", "this is t1   测试");
+        filedValueMap.put("t2", "this is t2    成功");
+        long begTime = System.currentTimeMillis();
+        boolean isSucc = new PdfGenerator().generatePdfByHtml(targetFilePath,new File("d:/aa.html"));
+        System.out.println("run time:"+(System.currentTimeMillis()-begTime));
+        System.out.println(isSucc);
+    }
+
+}
+

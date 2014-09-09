a851649276ed0c4199cc36966b234d70b5170614
skmbw <tongku2008@126.com>
2014-09-09 16:27:16 +0800

    durid分表

---
+++ test/src/main/java/com/alibaba/druid/mapping/spi/MappingProvider.java
@@ -0,0 +1,34 @@
+package com.alibaba.druid.mapping.spi;
+
+import java.util.List;
+
+import com.alibaba.druid.mapping.MappingContext;
+import com.alibaba.druid.mapping.MappingEngine;
+import com.alibaba.druid.sql.ast.SQLStatement;
+import com.alibaba.druid.sql.ast.statement.SQLDeleteStatement;
+import com.alibaba.druid.sql.ast.statement.SQLInsertStatement;
+import com.alibaba.druid.sql.ast.statement.SQLSelectQueryBlock;
+import com.alibaba.druid.sql.ast.statement.SQLUpdateStatement;
+import com.alibaba.druid.sql.visitor.ExportParameterVisitor;
+import com.alibaba.druid.sql.visitor.SQLASTOutputVisitor;
+
+public interface MappingProvider {
+
+    MappingVisitor createMappingVisitor(MappingEngine engine);
+    
+    MappingVisitor createMappingVisitor(MappingEngine engine, MappingContext context);
+    
+    ExportParameterVisitor createExportParameterVisitor(List<Object> parameters);
+
+    SQLASTOutputVisitor createOutputVisitor(MappingEngine engine, Appendable out);
+    
+    List<SQLStatement> explain(MappingEngine engine, String sql);
+
+    SQLSelectQueryBlock explainToSelectSQLObject(MappingEngine engine, String sql, MappingContext context);
+
+    SQLDeleteStatement explainToDeleteSQLObject(MappingEngine engine, String sql, MappingContext context);
+
+    SQLUpdateStatement explainToUpdateSQLObject(MappingEngine engine, String sql, MappingContext context);
+    
+    SQLInsertStatement explainToInsertSQLObject(MappingEngine engine, String sql, MappingContext context);
+}
---
+++ test/src/main/java/com/alibaba/druid/mapping/spi/MappingVisitor.java
@@ -0,0 +1,38 @@
+package com.alibaba.druid.mapping.spi;
+
+import java.util.List;
+import java.util.Map;
+
+import com.alibaba.druid.mapping.Entity;
+import com.alibaba.druid.mapping.MappingContext;
+import com.alibaba.druid.mapping.MappingEngine;
+import com.alibaba.druid.mapping.Property;
+import com.alibaba.druid.sql.ast.SQLExpr;
+import com.alibaba.druid.sql.ast.statement.SQLTableSource;
+import com.alibaba.druid.sql.visitor.SQLASTVisitor;
+
+public interface MappingVisitor extends SQLASTVisitor {
+    MappingEngine getEngine();
+    
+    List<Object> getParameters();
+    
+    MappingContext getContext();
+
+    Entity getEntity(String name);
+
+    Map<String, Entity> getEntities();
+
+    Map<String, SQLTableSource> getTableSources();
+    
+    String resolveTableName(Entity entity);
+    
+    String resovleColumnName(Entity entity, Property property);
+    
+    List<PropertyValue> getPropertyValues();
+    
+    int getAndIncrementVariantIndex();
+    
+    List<SQLExpr> getUnresolveList();
+    
+    void afterResolve();
+}

f74ee7eb191120d61ddb8cc8f487473b0ae8bc9f
skmbw <tongku2008@126.com>
2014-09-22 09:47:34 +0800

    异步任务

---
+++ test/src/main/java/org/apache/ibatis/executor/AsyncExecutor.java
@@ -0,0 +1,93 @@
+package org.apache.ibatis.executor;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.ibatis.executor.statement.StatementHandler;
+import org.apache.ibatis.logging.Log;
+import org.apache.ibatis.mapping.BoundSql;
+import org.apache.ibatis.mapping.MappedStatement;
+import org.apache.ibatis.session.Configuration;
+import org.apache.ibatis.session.ResultHandler;
+import org.apache.ibatis.session.RowBounds;
+import org.apache.ibatis.transaction.Transaction;
+
+public class AsyncExecutor extends BaseExecutor {
+	
+	protected AsyncExecutor(Configuration configuration, Transaction transaction) {
+		super(configuration, transaction);
+	}
+
+	public int doUpdate(MappedStatement ms, Object parameter)
+			throws SQLException {
+		Statement stmt = null;
+		try {
+			Configuration configuration = ms.getConfiguration();
+			StatementHandler handler = configuration.newStatementHandler(this,
+					ms, parameter, RowBounds.DEFAULT, null, null);
+			stmt = prepareStatement(handler, ms.getStatementLog());
+			return handler.update(stmt);
+		} finally {
+			closeStatement(stmt);
+		}
+	}
+	
+	public <E> List<E> doQuery(MappedStatement ms, Object parameter,
+			RowBounds rowBounds, ResultHandler resultHandler, BoundSql boundSql)
+			throws SQLException {
+		Statement stmt = null;
+		try {
+			Configuration configuration = ms.getConfiguration();
+			List<E> result = new ArrayList<E>();
+			List<String> sqlList = boundSql.getSqlList();
+			int size = sqlList.size();
+			if (size == 1) {// 只有一条sql直接在主线程执行
+				StatementHandler handler = configuration.newStatementHandler(
+						wrapper, ms, parameter, rowBounds, resultHandler,
+						boundSql);
+				stmt = prepareStatement(handler, ms.getStatementLog());
+				result = handler.<E> query(stmt, resultHandler);
+			} else {
+				CountDownLatch latch = new CountDownLatch(size);
+				for (String sql : sqlList) {// 这里使用多线程处理
+					latch.countDown();
+					boundSql.setSql(sql);
+					StatementHandler handler = configuration.newStatementHandler(
+							wrapper, ms, parameter, rowBounds, resultHandler,
+							boundSql);
+					stmt = prepareStatement(handler, ms.getStatementLog());
+					List<E> temp = handler.<E> query(stmt, resultHandler);
+					result.addAll(temp);
+				}
+				try {
+					latch.await(5, TimeUnit.SECONDS);// 5秒中超时
+				} catch (InterruptedException e) {
+					// 线程等待，中断异常
+				}
+			}
+			return result;
+		} finally {
+			closeStatement(stmt);
+		}
+	}
+	
+	public List<BatchResult> doFlushStatements(boolean isRollback)
+			throws SQLException {
+		return Collections.emptyList();
+	}
+
+	private Statement prepareStatement(StatementHandler handler,
+			Log statementLog) throws SQLException {
+		Statement stmt;
+		Connection connection = getConnection(statementLog);
+		stmt = handler.prepare(connection);
+		handler.parameterize(stmt);
+		return stmt;
+	}
+}

28b5d0c464fafae290f0555ab7da2830a851a86a
skmbw <tongku2008@126.com>
2014-10-28 16:23:12 +0800

    protobuf test

---
+++ test/src/test/java/com/vteba/test/proto/FirstProtoTest.java
@@ -0,0 +1,39 @@
+package com.vteba.test.proto;
+
+import org.junit.Test;
+
+import com.google.protobuf.InvalidProtocolBufferException;
+import com.vteba.protobuf.FirstProtobuf.SearchRequest;
+
+public class FirstProtoTest {
+	private static byte[] bytes;
+	
+	@Test
+	public void test() {
+		long d = System.currentTimeMillis();
+		SearchRequest.Builder builder = SearchRequest.newBuilder();
+		builder.setPageNumber(32);
+		builder.setQuery("yinlei尹雷");
+		builder.setResultPerPage(10);
+//		builder.setSamples(0, 2);
+		
+		SearchRequest searchRequest = builder.build();
+		bytes = searchRequest.toByteArray();
+		System.out.println(System.currentTimeMillis() - d);
+	}
+	
+	@Test
+	public void deser() {
+		long d = System.currentTimeMillis();
+		try {
+			SearchRequest searchRequest = SearchRequest.parseFrom(bytes);
+			searchRequest.getPageNumber();
+			searchRequest.getQuery();
+			searchRequest.getResultPerPage();
+			searchRequest.getSamplesList();
+		} catch (InvalidProtocolBufferException e) {
+			
+		}
+		System.out.println(System.currentTimeMillis() - d);
+	}
+}

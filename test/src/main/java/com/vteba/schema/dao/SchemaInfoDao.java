c597e7cdd8bd6b224d348433c906ceb43f27594d
skmbw <tongku2008@126.com>
2014-10-31 14:28:56 +0800

    添加分区表信息

--- test/src/main/java/com/vteba/schema/dao/SchemaInfoDao.java
+++ test/src/main/java/com/vteba/schema/dao/SchemaInfoDao.java
@@ -1,10 +1,12 @@
 package com.vteba.schema.dao;
 
 import com.vteba.schema.model.SchemaInfo;
+import com.vteba.service.tenant.annotation.Schema;
 import com.vteba.tx.jdbc.mybatis.annotation.DaoMapper;
 import com.vteba.tx.jdbc.params.DeleteBean;
 import com.vteba.tx.jdbc.params.QueryBean;
 import com.vteba.tx.jdbc.params.UpdateBean;
+
 import java.util.List;
 
 /**
@@ -13,6 +15,7 @@ import java.util.List;
  * @date 2014-10-31 13:58:51
  */
 @DaoMapper
+@Schema(name = "schema")
 public interface SchemaInfoDao {
     /**
      * 根据params所携带条件进行count计数。
